30a0a71907bbe06584556f56bd948682d1cce3d1
Bryan Keane <20089166@mail.wit.ie>
2020-11-16 16:45:38 +0000

    Second Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,4 +5,8 @@ public class Controller {
     public void testPrint(){
         System.out.println("TEST");
     }
+
+    public void testPrint2() {
+        System.out.println("Fuck YEAH");
+    }
 }
