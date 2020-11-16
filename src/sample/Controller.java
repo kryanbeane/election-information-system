a1fd725760d5e1e59666d31ce55089290e9fa500
Peter English <englishepeter29@gmail.com>
2020-11-16 16:53:39 +0000

    Third Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -9,4 +9,8 @@ public class Controller {
     public void testPrint2() {
         System.out.println("FUCK YEAH");
     }
+
+    public void testPrint3(){
+        System.out.println("I think we got it!");
+    }
 }
