8110021f937180f5704da7f844d6aa004cd1078b
Bryan Keane <20089166@mail.wit.ie>
2020-12-08 11:16:02 +0000

    Added getListIndex() method and searchPolitician() method

--- src/electionSystem/HashTable.java
+++ src/electionSystem/HashTable.java
@@ -2,7 +2,7 @@ package electionSystem;
 
 public class HashTable<T> {
 
-    List<T>[] hashTableArray;
+    List<T>[] hashTableArray;                                       // Need to figure out why this allows use of the linked list methods while it's an array.
 
     /**
      * Constructor.
@@ -15,37 +15,28 @@ public class HashTable<T> {
         }
     }
 
-    /**
-     *  Hashes a politician or a candidate name.
-     * @param key - Key to be hashed.
-     * @return - Remainder after dividing total by hash table size.
-     */
-    public int hashFunction(int key) {
-        // Value that will be mapped in the hash table.
-        int total=0;
-        // Adds the key to the total
-        total+=key;
+    // Hashes a politician or a candidate name.
+    public int hashFunction(String name) {                          // Hashes the names.
+        int total=0;                                                // Total is the value that will be mapped in the hash table. Sets initial total to 0.
+        for(int i=0; i<name.length(); i++) {                        // For loop to traverse through each character in the string of the name.
+            total+=name.charAt(i);                                  // Adds ASCII values of each char to total.
+        }
 
         System.out.println(total%hashTableArray.length);
-        return total%hashTableArray.length;
+        return total%hashTableArray.length;                         // Returns the remainder after dividing the total by the hash table size.
     }
 
-    /**
-     *  Insert the hashed person into the hash table.
-     * @param key -
-     * @param person -
-     */
-    public void insertHash(int key, T person) {                 // Insert the hashed person into the hash table.
-        hashTableArray[hashFunction(key)].addNode(person);
+    public void insertHash(String name, T person) {                 // Insert the hashed person into the hash table.
+        hashTableArray[hashFunction(name)].addNode(person);
     }
 
     /**
      * Removes a hash from the hash table.
-     * @param key - Name string of selected person to remove.
+     * @param name - Name string of selected person to remove.
      * @param person - Object of selected person to remove.
      */
-    public void removeHash(int key, T person) {
-        int node = hashFunction(key);
+    public void removeHash(String name, T person) {
+        int node = hashFunction(name);
         for(int i = 0; i < hashTableArray[node].length(); i++) {
             if(hashTableArray[node].accessAtIndex(i).getContents() == person) {
                 hashTableArray[node].removeNode(i);
@@ -55,24 +46,18 @@ public class HashTable<T> {
 
     /**
      * Edits an old hash and replaces it with a new hashed person.
-     * @param oldKey - Name of old person.
+     * @param oldName - Name of old person.
      * @param oldPerson - Object of old person.
-     * @param newKey - Name of new person to hash.
+     * @param newName - Name of new person to hash.
      * @param newPerson - Object of new person to hash.
      */
-    public void edit(int oldKey, T oldPerson, int newKey, T newPerson) {
-        removeHash(oldKey, oldPerson);
-        insertHash(newKey, newPerson);
+    public void edit(String oldName, T oldPerson, String newName, T newPerson) {
+        removeHash(oldName, oldPerson);
+        insertHash(newName, newPerson);
     }
 
     public T getHash(int hash, int element) {
         return hashTableArray[hash].accessAtIndex(element).getContents();
     }
-
-    ///Returns the hashtable size///
-    public int hashSize(int hash){
-        return hashTableArray[hash].length();
-    }
-
 
 }
